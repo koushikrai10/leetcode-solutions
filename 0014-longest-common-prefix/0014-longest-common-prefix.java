4fef35af35b2021733bbaf792c8cf1baef9f9a45
Koushik Rai <koushikraisullia@gmail.com>
2026-08-24 19:22:05 +0530

    Time: 0 ms (100%), Space: 43.3 MB (50.8%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String prefix=strs[0];
+        for(int i=1;i<strs.length;i++)
+        {
+            String current=strs[i];
+
+            while(!current.startsWith(prefix))
+            {
+                if(prefix.isEmpty())
+                {
+                    return "";
+                }
+
+               prefix=prefix.substring(0,prefix.length()-1); 
+            }
+        }
+        return prefix;
+    }
+}
