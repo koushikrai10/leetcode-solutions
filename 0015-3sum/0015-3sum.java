0c5a161b7064cd69b58c4171923f2349dfe723d5
Koushik Rai <koushikraisullia@gmail.com>
2026-09-10 20:49:36 +0530

    Time: 32 ms (76.46%), Space: 59 MB (85.82%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -25,11 +25,11 @@ class Solution {
                 left++;
                 right--;
 
-                while(left<right && nums[left]==nums[left+1])
+                while(left<right && nums[left]==nums[left-1])
                 {
                     left++;
                 }
-                while(left<right && nums[right]==nums[right-1])
+                while(left<right && nums[right]==nums[right+1])
                 {
                     right--;
                 }
