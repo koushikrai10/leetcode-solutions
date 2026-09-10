c5467bbd2b8bb41996fa61e4e9fe85dcd6bdf233
Koushik Rai <koushikraisullia@gmail.com>
2026-09-10 20:47:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -7,46 +7,44 @@ class Solution {
 
         for(int i=0;i<nums.length-2;i++)
         {
-            if(i>0 && nums[i]==nums[i-1])
-            {
+             if(i>0 && nums[i]==nums[i-1])
+             {
                 continue;
-            }
-        int left=i+1;
-        int right=nums.length-1;
+             }
 
-        while(left<right)
-        {
-            int sum=nums[i]+nums[left]+nums[right];
+             int left=i+1;
+             int right=nums.length-1;
 
-            if(sum==0)
-            {
+             while(left<right)
+             {
+                int sum=nums[i]+nums[left]+nums[right];
+                if(sum==0)
+                {
                 result.add(Arrays.asList(nums[i],nums[left],nums[right]));
 
                 left++;
                 right--;
 
-                while(left<right && nums[left]==nums[left-1])
+                while(left<right && nums[left]==nums[left+1])
                 {
                     left++;
                 }
-                while(left<right && nums[right]==nums[right+1])
+                while(left<right && nums[right]==nums[right-1])
                 {
                     right--;
                 }
-            }
-            else if(sum<0)
-            {
+             }
+             else if(sum<0)
+             {
                 left++;
-            }
-            else
-            {
+             }
+             else
+             {
                 right--;
-            }
-            
-        }
+             }
 
         }
+        }
         return result;
-        
     }
 }
